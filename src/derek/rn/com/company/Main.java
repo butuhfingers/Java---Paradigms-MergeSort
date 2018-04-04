cc9b757830e8844eb24d7e9ea51a75774db94e5c
derek <derek.crew@rocky.edu>
2018-04-04 10:03:31 -0600

    Finish product

--- src/derek/rn/com/company/Main.java
+++ src/derek/rn/com/company/Main.java
@@ -4,11 +4,14 @@ import java.util.Random;
 
 public class Main {
 
+    private static int low = 0;
+    private static int high = 5000000;
+
     public static void main(String[] args) {
 	// write your code here
         Random random = new Random();
-        int low = 0;
-        int high = 500000;
+
+        long startTime = System.currentTimeMillis();
 
         int array[] = new int[high];
         for(int i = 0;i < high;i++){
@@ -24,5 +27,7 @@ public class Main {
 
         System.out.println("\nSorted array");
 //        MergeSort.printArray(arr);
+
+        System.out.println("The sorting took: " + (System.currentTimeMillis()- startTime) + " milliseconds");
     }
 }
